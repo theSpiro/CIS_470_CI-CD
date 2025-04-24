e9459bbe0959f643ba7c845e420e1f4962f3f0ee
Joshua Tanguay <jtanguay@umassd.edu>
2025-04-23 20:14:32 -0400

    fixed reference error

--- compute.test.js
+++ compute.test.js
@@ -10,25 +10,25 @@ describe('classifyTriangle: Required input conditions -  See readme for more det
     
   
     test('should return Passed for subtraction', () => {
-      expect(checkSubtraction(7, 2, '-')).toBe(5);
+      expect(compute(7, 2, '-')).toBe(5);
       // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
     });
 
   
     test('should return Passed for division', () => {
-      expect(checkDivsion(4, 2, '/')).toBe(2);
+      expect(compute(4, 2, '/')).toBe(2);
       // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
     });
 
     
 
     test('should return Cannot Divide by 0 for division', () => {
-      expect(checkDivsion(4, 0, '/')).toBe('Cannot Divide By 0');
+      expect(compute(4, 0, '/')).toBe('Cannot Divide By 0');
       // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
     });
 
     test('should return Passed for mutiplication', () => {
-      expect(checkMutiplication(4, 2, '*')).toBe(8);
+      expect(compute(4, 2, '*')).toBe(8);
       // add more test cases based on your requirements and Boundary Value Analysis + Equivalence Partitioning
     });
 
